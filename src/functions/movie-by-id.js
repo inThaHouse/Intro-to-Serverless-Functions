689aaa79c045dcdd3a402951b7acf3ce37d0002f
Alan Nguy <alan.nguy@outlook.com>
2021-02-28 20:06:49 -0500

    commented a file to prevent bug

--- src/functions/movie-by-id.js
+++ src/functions/movie-by-id.js
@@ -1,18 +1,17 @@
-const movies = require('../../data/movies.json')
+// This was used with json data. This will need to be redone with gql query
+// exports.handler = async ({ queryStringParameters }) => {
+//   const { id } = queryStringParameters
+//   const movie = movies.find((movie) => movie.id === id)
 
-exports.handler = async ({ queryStringParameters }) => {
-  const { id } = queryStringParameters
-  const movie = movies.find((movie) => movie.id === id)
+//   if (!movie) {
+//     return {
+//       statusCode: 404,
+//       body: 'Not Found',
+//     }
+//   }
 
-  if (!movie) {
-    return {
-      statusCode: 404,
-      body: 'Not Found',
-    }
-  }
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(movie),
-  }
-}
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(movie),
+//   }
+// }
